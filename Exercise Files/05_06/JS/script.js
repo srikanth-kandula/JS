9ae81d6b2ed81f25bd3b84b53c645902ddfcff55
sk185488 <sk185488@ncr.com>
2020-03-21 18:20:33 +0530

    adding/removing DOM elements

--- Exercise Files/05_06/JS/script.js
+++ Exercise Files/05_06/JS/script.js
@@ -1,1 +1,16 @@
+var Figure = document.querySelector(".featured-image");
+var Image = Figure.querySelector("img");
+var altText = Image.getAttribute("alt");
+
+var figCaption = document.createElement("figCaption");
+var captionText = document.createTextNode(altText);
+
+figCaption.appendChild(captionText);
+
+//console.log(figCaption);
+Figure.appendChild(figCaption);
+
+//console.log(Figure);
+
+Image.removeAttribute("alt");
 
